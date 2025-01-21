3d145b1e1e5a9e3d989e3af1b03fbdc9cec23c95
Annabelle Bridgewaters <apbridge@purdue.edu>
2025-01-21 17:05:36 -0500

    update 1/21

---
+++ src/components/Card2.jsx
@@ -0,0 +1,21 @@
+import img from '../assets/headshot-woman.png';
+import '../styles/card.css';
+const Card2 = () => {
+    const name = 'Lily Smith';
+    const title = 'Software Engineer';
+    const email = 'b@b.com';
+
+    return (
+        <div className = "profile-card">
+            <div className = "profile-card__image">
+                <img src={img} alt={name} />
+            </div>
+            <div className = "profile-card__content">
+                <p>{name}</p>
+                <p>{title}</p>
+                <p><a href={'mailto:${email}'}>{email}</a></p>
+            </div>
+        </div>
+    );
+}
+export default Card2;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import '../styles/navbar.css';
+const NavBar = () => {
+    return (
+        <nav className = "navbar">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Profiles</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+export default NavBar;
