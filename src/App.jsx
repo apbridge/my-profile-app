48112b29e2869ebbfb8dd44058e93ebf042362c4
Annabelle Bridgewaters <apbridge@purdue.edu>
2025-01-23 17:04:50 -0500

    1/23

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,51 @@ import About from "./components/About";
 import Navbar from "./components/NavBar";
 import Card1 from "./components/Card1";
 import Card2 from "./components/Card2";
+import image_man from "./assets/headshot-woman.png";
+import image_woman from "./assets/headshot-woman2.png";
+import Card from "./components/Card";
 
 const App = () => {
+  const profiles =[
+    {
+      img: image_man,
+      name:'John Doe',
+      title:'Software Engineer',
+      email:'a@a.com'
+    },
+    {
+      img: image_man,
+      name:'Lily Smith',
+      title:'Software Engineer',
+      email:'b@b.com'
+    },
+  ];
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
   return (
     <>
      <header>
         <Navbar />
      </header>
      <main>
-        <div className="section">
-          <div className="container">
-            <h1>Profile App</h1>
+       <Wrapper>
+          <h1>Profile App</h1>
+          <button onClick={handleClick}>
+            {clicked ? "Click me" : "Clicked"}
+          </button>
+       </Wrapper>
+      <Wrapper>
+          <About />
+      </Wrapper>
+      <Wrapper>
+          <div className="profile-cards">
+            {profiles.map((profile) => {
+              <Card key={profile.email} {...profile} />
+            })}
           </div>
-        </div>
-        <div className="section">
-          <div className="container">
-            <About />
-          </div>
-        </div>
-        <div className="section">
-          <div className="container">
-            <div className="profile-cards">
-              <Card1 />
-              <Card2 />
-            </div>
-          </div>
-        </div>
+      </Wrapper>
      </main>
     </>
   )
